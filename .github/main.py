230440322237323c00d848ee0fc729b86b157328
me-arissa <laa.ferreiraaugusto@gmail.com>
2026-05-22 17:56:11 -0300

    corrigi formatação exigida pelo flake8, CRLF > LF

--- .github/main.py
+++ .github/main.py
@@ -1,5 +1,6 @@
 def soma(a, b):
     return a + b
 
+
 if __name__ == "__main__":
     print("Resultado:", soma(2, 3))
--- .github/test_main.py
+++ .github/test_main.py
@@ -1,5 +1,6 @@
 from main import soma
 
+
 def test_soma():
     assert soma(2, 3) == 5
     assert soma(-1, 1) == 0
